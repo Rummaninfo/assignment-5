fc8265b30e2cec26e7d556849436cec4840b9eb9
Rummaninfo <birsabbir38@gmail.com>
2025-08-28 02:31:22 -0700

    call button

--- link.js
+++ link.js
@@ -1,3 +1,5 @@
+// Clicking on the 💗 heart icon of any card will increase the count in the Navbar
+//  Heart Icons
 let btn = document.getElementsByClassName("heart-icon")
 
 for(let button of btn){
@@ -8,5 +10,41 @@ for(let button of btn){
     })
 }
 
+// Call Buttons 
 
+let callBtn = document.getElementsByClassName("call-btn")
+for(let allBtn of callBtn){
+    allBtn.addEventListener('click', function(){
+
+       
+        let name = allBtn.parentNode.parentNode.childNodes[5].innerText
+        let num = allBtn.parentNode.parentNode.childNodes[7].innerText
+        // alert(name + " " +  num)
+
+        let coin = document.getElementById("coin").innerText
+        let parseCoin = parseInt(coin)
+        
+         if (parseCoin < 20){
+        alert('আপনার পর্যাপ্ত কয়েন নেই কল করতে কমপক্ষে   কয়েন লাগবে ।')
+        return
+       }
+       let minus = parseCoin - 20
+        document.getElementById("coin").innerText = minus
+        alert(name + " " +  num)
+       
+       
+        
+
+
+        
+
+
+        
+       
+
+
+
+
+    })
+}
 
